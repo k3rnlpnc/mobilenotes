3de6f7ec77bd99a1a4f62651bc86e1aa25b51d37
Angelina Shurygina <rocknrolllb20@gmail.com>
2023-04-07 09:54:54 +0300

    Added logging

--- app/src/main/java/com/shurygina/mobilenotes/MainActivity.kt
+++ app/src/main/java/com/shurygina/mobilenotes/MainActivity.kt
@@ -2,10 +2,44 @@ package com.shurygina.mobilenotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "onCreate was started")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("MainActivity", "onResume was started")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart was started")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause was started")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy was started")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop was started")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivity", "onRestart was started")
     }
 }
