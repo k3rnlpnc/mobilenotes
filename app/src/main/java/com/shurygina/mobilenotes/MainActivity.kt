ac17c5be7d3da381318137ab7124e94b7bfa112d
Angelina Shurygina <rocknrolllb20@gmail.com>
2023-04-12 10:47:50 +0300

    #1 Added listener on empty field click, added text input for tasks

--- app/src/main/java/com/shurygina/mobilenotes/MainActivity.kt
+++ app/src/main/java/com/shurygina/mobilenotes/MainActivity.kt
@@ -4,22 +4,43 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.view.Window
+import android.view.WindowManager
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Date
 
+@RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
+    lateinit var tasks: List<TextInputEditText>
+    lateinit var currentDate: TextView
+
+    val dateFormat = DateTimeFormatter.ofPattern("d MMMM")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
 
-        val currentDate = findViewById<TextView>(R.id.currentDate)
-        val formatter = DateTimeFormatter.ofPattern("d MMMM")
-        currentDate.text = LocalDate.now().format(formatter)
+        initStartActivity()
     }
+
+    private fun initStartActivity() {
+        currentDate = findViewById<TextView>(R.id.currentDate)
+        currentDate.text = LocalDate.now().format(dateFormat)
+    }
+
+    fun onAddTaskClick(view: View) {
+    }
+
+    fun onWindowClick(view: View) {
+        findViewById<ConstraintLayout>(R.id.mainActivity).clearFocus()
+    }
+
 }
